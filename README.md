b10792d07353f2d1a4a2db0d2eb813f8b92d2228
lzb <lzb@admin.com>
2017-01-13 11:23:49 +0800

    完善骨架

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Angular Boilerplate
-
-http-server 0.8.x 和 node 5.7 以上的版本不兼容
-
-http://www.html5rocks.com/zh/tutorials/file/xhr2/
+angular 项目骨架
--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
-(function(angular) {
-  'use strict';
+'use strict';
 
-  // 主模块
-  angular.module('moviecat', [
-    'ngRoute',
-    'moviecat.in_theaters',
-    'moviecat.comming_soon',
-  ])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.otherwise({ redirectTo: '/in_theaters' });
-  }]);
-
-}(angular));
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
